f799fd84b5a99e472892d0faa2b6a36d81ff7229
Nathan <nmcaleese87@gmail.com>
2022-06-05 07:17:02 -0700

    Adds js constants

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 /*----- constants -----*/
+const PLAYER1 = 'The Red Warriors';
+const PLAYER2 = 'The Blue Warriors';
 
+//set constants for different cards [note: card names may be used for random index if they're replaced with the score they hold]
+const CARDS = {
+    card1: {name: 'Celt  Warrior', score: 1},
+    card2: {name: 'Gladiator', score: 2},
+    card3: {name: 'English Knight', score: 3},
+    card4: {name: 'Viking', score: 4},
+    card5: {name: 'Moari Warrior', score: 5},
+    card6: {name: 'Zande Warrior', score: 6},
+    card7: {name: 'Persian Immortal', score: 7},
+    card8: {name: 'Ninja', score: 8},
+    card9: {name: 'Apache', score: 9},
+    card10: {name: 'Shaolin Monk', score: 10},
+    card11: {name: 'Samurai', score: 11},
+    card12: {name: 'Spartan', score: 12},
+}
 
 
 /*----- app's state (variables) -----*/
